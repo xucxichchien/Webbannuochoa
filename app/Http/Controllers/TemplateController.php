21354f3a54531a8d61c4c7e4e2b383fed40df9bb
xucxichchien <111264991+xucxichchien@users.noreply.github.com>
2022-08-15 20:45:51 +0700

    scd

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class TemplateController extends Controller
 {
     public function index(){
-     return view('Frontend.home')   ;
+     return view('FrontEnd.home')   ;
     }
 }
